342ba726e2d1f38b29fd8a499e95ef48a1a6b589
krypten <chaitiagrawal@gmail.com>
2017-06-17 20:34:47 +0530

    refactor: Moved the GridView to recyclerView
    Resolves #8

--- app/src/main/java/com/cyril/udacity/moviepop/model/MovieAdapter.java
+++ app/src/main/java/com/cyril/udacity/moviepop/model/MovieAdapter.java
@@ -1,14 +1,16 @@
 package com.cyril.udacity.moviepop.model;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.cyril.udacity.moviepop.MovieDetailActivity;
 import com.cyril.udacity.moviepop.R;
 import com.cyril.udacity.moviepop.client.TheMovieDbApi;
 import com.squareup.picasso.Picasso;
@@ -19,66 +21,76 @@ import java.util.List;
 /**
  * Created by agrawac on 12/3/16.
  */
-public class MovieAdapter extends ArrayAdapter {
-    private static final String LOG_TAG = MovieAdapter.class.getSimpleName();
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+	private static final String LOG_TAG = MovieAdapter.class.getSimpleName();
 
-    private Context mContext;
-    private List<Movie> mMovieList;
-    private LayoutInflater inflater;
+	private Context mContext;
+	private List<Movie> mMovieList;
+	private LayoutInflater inflater;
 
-    public MovieAdapter(final Context context, final int resourceId, final List<Movie> movies) {
-        super(context, resourceId, movies);
-        mContext = context;
-        mMovieList = new ArrayList<>(movies);
-        inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
+	public MovieAdapter(final Context context, final List<Movie> movies) {
+		mContext = context;
+		mMovieList = new ArrayList<>(movies);
+		inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
 
-    @Override
-    public int getCount() {
-        if (mMovieList != null) {
-            return mMovieList.size();
-        }
-        return 0;
-    }
+	@Override
+	public MovieAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		final View layoutView = inflater.inflate(R.layout.gridview_item_movie, parent, false);
+		final ViewHolder vh = new ViewHolder(layoutView);
+		layoutView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(final View view) {
+				final Intent intent = new Intent(view.getContext(), MovieDetailActivity.class);
+				intent.putExtra(Movie.PARCELABLE_ID, mMovieList.get(vh.getAdapterPosition()));
+				mContext.startActivity(intent);
+			}
+		});
+		return vh;
+	}
 
-    @Override
-    public Movie getItem(int i) {
-        if (mMovieList != null) {
-            return mMovieList.get(i);
-        }
-        return null;
-    }
+	@Override
+	public void onBindViewHolder(MovieAdapter.ViewHolder holder, int i) {
+		final String posterUrl = TheMovieDbApi.getPosterUrl(mMovieList.get(i).getPosterlUrl(), TheMovieDbApi.SIZE.MEDIUM);
+		Log.i(LOG_TAG, " Poster Url : " + posterUrl);
+		Picasso.with(mContext).load(posterUrl).into(holder.mPosterView);
+	}
 
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
+	@Override
+	public long getItemId(int i) {
+		return 0;
+	}
 
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        if (view == null) {
-            view = inflater.inflate(R.layout.gridview_item_movie, viewGroup, false);
-        }
-        final ImageView posterView = (ImageView) view.findViewById(R.id.gridview_item_poster);
-        final String posterUrl = TheMovieDbApi.getPosterUrl(mMovieList.get(i).getPosterlUrl(), TheMovieDbApi.SIZE.MEDIUM);
-        Log.i(LOG_TAG, " Poster Url : " + posterUrl);
-        Picasso.with(mContext).load(posterUrl).into(posterView);
-        return view;
-    }
+	@Override
+	public int getItemCount() {
+		if (mMovieList != null) {
+			return mMovieList.size();
+		}
+		return 0;
+	}
 
-    public List<Movie> getMovieList() {
-        return mMovieList;
-    }
+	public List<Movie> getMovieList() {
+		return mMovieList;
+	}
 
-    public void initialize(final List<Movie> result) {
-        if (result != null) {
-            mMovieList.clear();
-            for (final Movie movie : result) {
-                mMovieList.add(movie);
-            }
-            notifyDataSetChanged();
-        } else {
-            Toast.makeText(mContext, "INTERNET CONNECTION NOT PRESENT", Toast.LENGTH_LONG).show();
-        }
-    }
+	public void initialize(final List<Movie> result) {
+		if (result != null) {
+			mMovieList.clear();
+			for (final Movie movie : result) {
+				mMovieList.add(movie);
+			}
+			notifyDataSetChanged();
+		} else {
+			Toast.makeText(mContext, "INTERNET CONNECTION NOT PRESENT", Toast.LENGTH_LONG).show();
+		}
+	}
+
+	public class ViewHolder extends RecyclerView.ViewHolder {
+		public ImageView mPosterView;
+
+		public ViewHolder(final View view) {
+			super(view);
+			mPosterView = (ImageView) view.findViewById(R.id.gridview_item_poster);
+		}
+	}
 }
